099961f3ebd63e88e66e5e36c70d8c90f28ee574
Brett <bmb7904@gmail.com>
2021-11-12 11:55:28 -0500

    Update Program.fs
    Updated Program November 12, 2021

--- Program.fs
+++ Program.fs
@@ -1,3 +1,7 @@
+// Eric Cool and Brett Bernardi
+// Organization of Programming Languages 
+// Project
+
 module OPL.Project.GeneratePrimes
 
 open System
@@ -7,17 +11,22 @@ let isDivisible n1 n2 =
     // modulus check
     n1 % n2 = 0
 
+// Function that will calculate and return the square root of the Integer
+// argument, and return an integer value. 
+let sqrttoint n =
+    (int (sqrt (float n)))
+
 // Function that determines if `number` is a prime
 let isPrime number =
     // list of numbers ranging from `12` => `(number / 2) + 1`
+    let max = sqrttoint number
     let range =
         // Create 2 lists of
         // - the number - ex: [ 10 ]
         // - numbers up to half the number - ex: [ 1, 2, 3, 4, 5 ]
         // and join them together - ex: [ 10, 1, 2, 3, 4, 5 ]
-        List.append [ number ] [ 1 .. ((number / 2)) ]
+        [ 2 .. max ]
         // Filter the list by unique values (for #2)
-        |> List.distinct
 
     // allow count to be changed (mutable)
     let mutable count = 0
@@ -29,32 +38,45 @@ let isPrime number =
             // increment the counter
             count <- count + 1
     // true/false
-    count = 2 // if `count == 0` then it is a prime number
+    count = 0 // if `count == 0` then it is a prime number
 
 // Function to generate a list of primes up to the given `number`
 let generatePrimes number =
     // Filter a list from [`2` => `number`] if they are prime
     List.filter isPrime [ 2 .. number ]
 
+// Function that will print out a list, with a new line inserted after
+// every 10 elements
+let printList list = 
+  let mutable count = 1
+  for num in list do
+    printf "%5d " num
+    if count % 10 = 0 then
+      printf "\n"
+    count <- count + 1
+    
+
+
 // Main Entry to the Application
 [<EntryPoint>]
 let main argv =
-    printfn "Generate Prime Numbers"
+    printfn "Generate Prime Numbers\n\n"
 
-    // Check that the program was given the needed number of arguments
-    if argv.Length = 1 then
-        // Number to generate up to
-        let mutable number = 0
+    printf "Find prime numbers from 1 - "
 
-        // Attempt to parse the argument to an Integer
-        if Int32.TryParse(argv.[0], &number) then
-            printfn "Find prime numbers up to: %d" number
-            // Generate a list of primes and print them
-            printfn "%A" (generatePrimes number)
-        else
-            printfn "[ERROR] <number> argument could not be parsed."
+    // Reads the input from the user
+    let mutable max = System.Console.ReadLine()
 
-        0 // success
-    else
-        printfn "[ERROR] Please run application with: `PrimeNumberGenerator.exe <number>`"
-        1 // error
+    // Attempt to parse the user argument (max), storing it as an int
+    // into number
+    let mutable number = 0
+    if Int32.TryParse(max, &number) then
+      printfn "Find prime numbers up to: %d" number
+        
+      // Generate a list of primes and print them
+      printList (generatePrimes number)
+      0
+    else 
+      printfn "Error parsing input!"
+      1
+
