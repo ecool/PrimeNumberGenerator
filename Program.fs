772d6de9d758dc04d83b91045f98be6c0cfeb584
Eric Cool <coolazura@gmail.com>
2021-11-14 19:47:58 -0500

    clean up comments, auto format

--- Program.fs
+++ Program.fs
@@ -1,5 +1,5 @@
 // Eric Cool and Brett Bernardi
-// Organization of Programming Languages 
+// Organization of Programming Languages
 // Project
 
 module OPL.Project.GeneratePrimes
@@ -12,21 +12,16 @@ let isDivisible n1 n2 =
     n1 % n2 = 0
 
 // Function that will calculate and return the square root of the Integer
-// argument, and return an integer value. 
-let sqrttoint n =
-    (int (sqrt (float n)))
+// argument, and return an integer value.
+let sqrttoint n = (int (sqrt (float n)))
 
 // Function that determines if `number` is a prime
 let isPrime number =
     // list of numbers ranging from `12` => `(number / 2) + 1`
     let max = sqrttoint number
-    let range =
-        // Create 2 lists of
-        // - the number - ex: [ 10 ]
-        // - numbers up to half the number - ex: [ 1, 2, 3, 4, 5 ]
-        // and join them together - ex: [ 10, 1, 2, 3, 4, 5 ]
-        [ 2 .. max ]
-        // Filter the list by unique values (for #2)
+
+    // Create a list of numbers from `2` to `max`
+    let range = [ 2 .. max ]
 
     // allow count to be changed (mutable)
     let mutable count = 0
@@ -47,14 +42,14 @@ let generatePrimes number =
 
 // Function that will print out a list, with a new line inserted after
 // every 10 elements
-let printList list = 
-  let mutable count = 1
-  for num in list do
-    printf "%5d " num
-    if count % 10 = 0 then
-      printf "\n"
-    count <- count + 1
-    
+let printList list =
+    let mutable count = 1
+
+    for num in list do
+        printf "%5d " num
+        if count % 10 = 0 then printf "\n"
+        count <- count + 1
+
 
 
 // Main Entry to the Application
@@ -65,28 +60,28 @@ let main argv =
     printf "Find prime numbers from 1 - "
 
     // Reads the input from the user
-    let mutable max = System.Console.ReadLine()
+    let max = System.Console.ReadLine()
 
-    // Attempt to parse the user argument (max), storing it as an int
+    // Attempt to parse the user input (max), storing it as an int
     // into number
     let mutable number = 0
+
     if Int32.TryParse(max, &number) then
-      printfn "Find prime numbers up to: %d" number
+        printfn "Find prime numbers up to: %d" number
 
-      // Generate a list of primes and print them
-      let primeList = generatePrimes number
-      
-      // Get size of list of primes (number of primes calculated)
-      let numOfPrimes = List.length primeList
-      
-      // Print out list of primes
-      printList primeList
+        // Generate a list of primes and print them
+        let primeList = generatePrimes number
 
-      // Print out size of list of primes
-      printf "\n\n\nYou generated %d of primes!\n\n\n" numOfPrimes
+        // Get size of list of primes (number of primes calculated)
+        let numOfPrimes = List.length primeList
 
-      0
-    else 
-      printfn "Error parsing input!"
-      1
+        // Print out list of primes
+        printList primeList
 
+        // Print out size of list of primes
+        printf "\n\n\nYou generated %d of primes!\n\n\n" numOfPrimes
+
+        0
+    else
+        printfn "Error parsing input!"
+        1
